b75dff7ad8ae88a370b717649a39c4543e0d37a0
Sid.Zhang <sid.zhang@asurion.com>
2022-06-22 16:46:27 +0900

    number,string,boolean

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Time to get started ...');
+function add(n1, n2, showResult, phrase) {
+    if (showResult) {
+        console.log(phrase + n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var result = add(number1, number2, true, "result is");
+console.log(result);
